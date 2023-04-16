9edddc047bfe20b09a69f670588c51be0ce57276
Felipe Larson da Silveira <felipe.silveira@enline-transmission.com>
2023-04-16 09:15:36 +0100

    feat: added luxon to added datetime

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,41 +3,47 @@ import {
   HeaderRealtime,
   HeaderUserMenu,
   HeaderWraper,
-} from './styles'
-import Image from 'next/image'
+} from "./styles";
+import Image from "next/image";
 
-import logo from '../../Assets/logo-white.svg'
-import { Avatar } from '@enline-ui/react'
-import { useRouter } from 'next/router'
+import logo from "../../Assets/logo-white.svg";
+import { Avatar } from "@enline-ui/react";
+import { useRouter } from "next/router";
+
+import { DateTime } from "luxon";
 
 export const Header = () => {
-  const router = useRouter()
+  const router = useRouter();
+
   return (
     <>
       <HeaderContainer>
         <HeaderWraper>
           <Image
-            style={{ cursor: 'pointer' }}
-            onClick={() => router.push('/')}
+            style={{ cursor: "pointer" }}
+            onClick={() => router.push("/")}
             src={logo}
             alt="logo"
             width={95}
             height={25}
           />
-          <HeaderRealtime size={'xs'}>
-            Real-time: Apr 06, 20:57:21, UTC +0
+          <HeaderRealtime size={"xs"}>
+            Real-time:{" "}
+            {DateTime.now()
+              .toFormat("MMMM dd, HH:mm ZZZZ")
+              .replace("GMT", "UTC")}
           </HeaderRealtime>
         </HeaderWraper>
         <HeaderWraper>
           <Avatar
             src="https://files-enline-dev.s3.amazonaws.com/company-logo/MMG.png"
-            containerProps={{ avatarType: 'rounded' }}
+            containerProps={{ avatarType: "rounded" }}
           />
-          <HeaderUserMenu as="strong" size={'sm'}>
+          <HeaderUserMenu as="strong" size={"sm"}>
             Hello, Andre da MMG
           </HeaderUserMenu>
         </HeaderWraper>
       </HeaderContainer>
     </>
-  )
-}
+  );
+};
