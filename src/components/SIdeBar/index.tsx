983666fdcc66d2c143d5029e380a6a4ad18ac619
Felipe Larson da Silveira <felipe.silveira@enline-transmission.com>
2023-04-16 23:10:00 +0100

    feat: added icons

--- src/components/SIdeBar/index.tsx
+++ src/components/SIdeBar/index.tsx
@@ -1,7 +1,57 @@
 import { Container } from "./styles";
 
+import { Heading, Button } from "@enline-ui/react";
+import {
+  Broadcast,
+  ChartLine,
+  Cube,
+  Faders,
+  Gauge,
+  Lifebuoy,
+  MapTrifold,
+  Warning,
+} from "@phosphor-icons/react";
+
 export const SideBar = () => (
   <Container>
-    <h1>SideBar</h1>
+    <Heading>Company</Heading>
+    <hr />
+    <>
+      <Button variant={"primaryLeftBar"}>
+        <Gauge size={32} />
+        Cockpit
+      </Button>
+      <Button variant={"primaryLeftBar"}>
+        <Cube size={32} />
+        Digital Twin
+      </Button>
+      <Button variant={"primaryLeftBar"}>
+        <MapTrifold size={32} />
+        Maps
+      </Button>
+      <Button variant={"primaryLeftBar"}>
+        <ChartLine size={32} />
+        Data Analisys
+      </Button>
+      <Button variant={"primaryLeftBar"}>
+        <Warning size={32} />
+        Alerts
+      </Button>
+      <Button variant={"primaryLeftBar"}>
+        <Broadcast size={32} />
+        Fault location
+      </Button>
+    </>
+
+    <>
+      <Button variant={"primaryLeftBar"}>
+        <Faders size={32} />
+        Settings
+      </Button>
+      <Button variant={"primaryLeftBar"}>
+        <Lifebuoy size={32} />
+        Need help?
+      </Button>
+    </>
   </Container>
 );
--- src/components/SIdeBar/styles.ts
+++ src/components/SIdeBar/styles.ts
@@ -1,9 +1,13 @@
-import { styled } from "@enline-ui/react";
+import { Box, styled } from "@enline-ui/react";
 
-export const Container = styled("div", {
+export const Container = styled(Box, {
   height: "calc(100vh - $12)",
-  width: "20rem",
-  background: "$gray100",
+  width: "$64",
+  background: "$white",
   display: "flex",
   flexDirection: "column",
+  gap: "$3",
+  padding: "$4",
+  zIndex: "1",
+  boxShadow: "0px 0px 30px rgba(0, 0, 0, 0.25)",
 });
