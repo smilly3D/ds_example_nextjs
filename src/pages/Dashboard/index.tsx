77a4103d504ab38ac76175ee1ad06b056dfd79c9
Felipe Larson da Silveira <felipe.silveira@enline-transmission.com>
2023-04-23 05:53:08 +0100

    feat: created switch

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,14 +7,36 @@ import {
 
 import { Kpis } from "@/components/Kpis";
 
-import { Heading } from "@enline-ui/react";
+import { Heading, Switch, Text } from "@enline-ui/react";
 
 export default function Home() {
   return (
     <>
       <Container>
         <ContainerHeader>
-          <Heading as="h2">Header</Heading>
+          <div
+            style={{
+              width: "fit-content",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "0 2rem",
+            }}
+          >
+            <div
+              style={{
+                border: "1px solid red",
+                padding: "0.5rem 1rem",
+                display: "flex",
+                gap: "1rem",
+                borderRadius: "0.5rem",
+              }}
+            >
+              <Switch /> <Text size={"xs"}>Circuit 1</Text>
+              <Switch /> <Text size={"xs"}>Circuit 2</Text>
+            </div>
+          </div>
         </ContainerHeader>
         <ContainerBody>
           <Heading as="h2">Body</Heading>
